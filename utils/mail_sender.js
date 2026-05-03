43ddb39cc38c78c857811b4c0bad9100a93b56ea
Mihir Lathiya <mihir.lathiya@techholding.co>
2026-05-03 18:11:03 +0530

    fix: replace nodemailer with Resend to fix SMTP timeout on Railway
    Railway blocks outbound SMTP (ports 465/587). Switched to Resend HTTP API
    which works reliably on cloud platforms with no firewall issues.

--- utils/mail_sender.js
+++ utils/mail_sender.js
@@ -1,51 +1,30 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 const logger = require('./logger');
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 function maskEmail(email) {
   const [user, domain] = email.split('@');
   return `${user.slice(0, 2)}***@${domain}`;
 }
 
-function mailSender(to, subject, text, html) {
+async function mailSender(to, subject, text, html) {
   const masked = maskEmail(to);
+  logger.info('MAIL', 'Sending email', { to: masked, subject });
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.SYSTEM_MAIL_ID ? process.env.SYSTEM_MAIL_ID.replace(/['"\s]/g, '') : '',
-      pass: process.env.SYSTEM_MAIL_PASS ? process.env.SYSTEM_MAIL_PASS.replace(/['"\s]/g, '') : '',
-    },
-  });
-
-  const mailOptions = {
-    from: process.env.SYSTEM_MAIL_ID,
+  const { data, error } = await resend.emails.send({
+    from: process.env.SYSTEM_MAIL_FROM || 'PDMS <noreply@yourdomain.com>',
     to,
     subject,
     text: text || '',
     html: html || '',
-  };
-
-  logger.info('MAIL', 'Sending email', { to: masked, subject });
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      logger.error('MAIL', 'Failed to send email', {
-        to: masked,
-        subject,
-        error: error.message,
-        code: error.code,
-      });
-    } else {
-      logger.info('MAIL', 'Email delivered', {
-        to: masked,
-        subject,
-        messageId: info.messageId,
-        response: info.response,
-      });
-    }
   });
+
+  if (error) {
+    logger.error('MAIL', 'Failed to send email', { to: masked, subject, error: error.message });
+  } else {
+    logger.info('MAIL', 'Email delivered', { to: masked, subject, messageId: data.id });
+  }
 }
 
 module.exports = mailSender;
